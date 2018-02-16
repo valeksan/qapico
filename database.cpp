61b44b8a2e4e3a17c752e26c7b4a3b2e8fd654ba
vi <zenix@cn.ru>
2018-02-16 17:57:35 +0700

    complete functions to make tables

--- database.cpp
+++ database.cpp
@@ -70,10 +70,8 @@ bool DataBase::createAllTables()
 {
     if((!this->createCurrenciesTable())
             || (!this->createAppAreasTable())
-            || (!this->createContactsTable())
             || (!this->createDevInfoTable())
             || (!this->createGithubHistoryPoolTable())
-            || (!this->createInfoTable())
             || (!this->createMarketsTable())
             || (!this->createRoadmapDatesPoolTable())
             || (!this->createTechnologiesTable())
@@ -85,67 +83,202 @@ bool DataBase::createAllTables()
 }
 
 bool DataBase::createCurrenciesTable()
-{
-    //
-    return true;
-}
-
-bool DataBase::createInfoTable()
-{
-    //
-    return true;
+{    
+    QSqlQuery query;
+    if(!query.exec( "CREATE TABLE " T_CURRENCIES " ("
+                            "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                            T_CURRENCIES_NAME " VARCHAR(255) NOT NULL,"
+                            T_CURRENCIES_SYMBOL " VARCHAR(10) NOT NULL,"
+                            T_CURRENCIES_CALC_SUPPLY " UNSIGNED BIG INT NOT NULL,"
+                            T_CURRENCIES_VOLUME_USD " UNSIGNED BIG INT NOT NULL,"
+                            T_CURRENCIES_VOLUME_BTC " UNSIGNED BIG INT NOT NULL,"
+                            T_CURRENCIES_PRICE_USD " INTEGER NOT NULL,"
+                            T_CURRENCIES_PRICE_BTC " INTEGER NOT NULL,"
+                            T_CURRENCIES_MINEABLE " BOOLEAN NOT NULL"
+                            T_CURRENCIES_MANUAL_UPD_DATE " DATE NOT NULL,"
+                            T_CURRENCIES_APP_AREAS_ID " INTEGER,"
+                            T_CURRENCIES_REV_IDEA " TEXT,"
+                            T_CURRENCIES_BASE_PLATFORM_ID " INTEGER,"
+                            T_CURRENCIES_CON_ALG_ID " INTEGER,"
+                            T_CURRENCIES_IS_INTER_COMP " BOOLEAN,"
+                            T_CURRENCIES_IS_WP_IN_CV " BOOLEAN,"
+                            T_CURRENCIES_IS_OP_VACANCIES " BOOLEAN,"
+                            T_CURRENCIES_WP_URL " TEXT,"
+                            T_CURRENCIES_RM_URL " TEXT,"
+                            T_CURRENCIES_SITE_URL " TEXT,"
+                            T_CURRENCIES_CONTACTS_TEL " VARCHAR(30),"
+                            T_CURRENCIES_CONTACTS_MAIL " VARCHAR(100),"
+                            T_CURRENCIES_CONTACTS_ADDRESS " TEXT,"
+                            T_CURRENCIES_CONTACTS_SLACK " VARCHAR(100),"
+                            T_CURRENCIES_CONTACTS_TELEGRAM " VARCHAR(100)"
+                        " )"
+                    )) {
+        qDebug() << "DataBase: error of create " << T_CURRENCIES;
+        qDebug() << query.lastError().text();
+        return false;
+    } else {
+        return true;
+    }
+    return false;
 }
 
 bool DataBase::createRoadmapDatesPoolTable()
 {
-    //
-    return true;
+    QSqlQuery query;
+    if(!query.exec( "CREATE TABLE " T_ROADMAPSDATESPOOL " ("
+                            "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                            T_ROADMAPSDATESPOOL_DATE " DATE NOT NULL,"
+                            T_ROADMAPSDATESPOOL_EVENT_INFO " TEXT"
+                        " )"
+                    )) {
+        qDebug() << "DataBase: error of create " << T_ROADMAPSDATESPOOL;
+        qDebug() << query.lastError().text();
+        return false;
+    } else {
+        return true;
+    }
+    return false;
 }
 
 bool DataBase::createTokenAlgTable()
 {
-    //
-    return true;
+    QSqlQuery query;
+    if(!query.exec( "CREATE TABLE " T_TOKENALG " ("
+                            "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                            T_TOKENALG_NAME " VARCHAR(30) NOT NULL,"
+                            T_TOKENALG_INFO " TEXT"
+                        " )"
+                    )) {
+        qDebug() << "DataBase: error of create " << T_TOKENALG;
+        qDebug() << query.lastError().text();
+        return false;
+    } else {
+        return true;
+    }
+    return false;
 }
 
 bool DataBase::createAppAreasTable()
 {
-    //
-    return true;
+    QSqlQuery query;
+    if(!query.exec( "CREATE TABLE " T_AREAS " ("
+                            "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                            T_AREAS_NAME " VARCHAR(50) NOT NULL"
+                        " )"
+                    )) {
+        qDebug() << "DataBase: error of create " << T_AREAS;
+        qDebug() << query.lastError().text();
+        return false;
+    } else {
+        return true;
+    }
+    return false;
 }
 
 bool DataBase::createDevInfoTable()
 {
-    //
-    return true;
+    QSqlQuery query;
+    if(!query.exec( "CREATE TABLE " T_DEVINFO " ("
+                            "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                            T_DEVINFO_TEAM_SIZE " INTEGER NOT NULL,"
+                            T_DEVINFO_START_DATE " INTEGER NOT NULL"
+                            T_DEVINFO_LANGUAGES_USE " TEXT NOT NULL"
+                        " )"
+                    )) {
+        qDebug() << "DataBase: error of create " << T_DEVINFO;
+        qDebug() << query.lastError().text();
+        return false;
+    } else {
+        return true;
+    }
+    return false;
 }
 
 bool DataBase::createTechnologiesThreadsPoolTable()
 {
-    //
-    return true;
+    QSqlQuery query;
+    if(!query.exec( "CREATE TABLE " T_TECHPOOL " ("
+                            "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                            T_TECHPOOL_CURRENCY_ID " INTEGER NOT NULL,"
+                            T_TECHPOOL_TECHNOLOGY_ID " INTEGER NOT NULL"
+                        " )"
+                    )) {
+        qDebug() << "DataBase: error of create " << T_TECHPOOL;
+        qDebug() << query.lastError().text();
+        return false;
+    } else {
+        return true;
+    }
+    return false;
 }
 
 bool DataBase::createTechnologiesTable()
 {
-    //
-    return true;
+    QSqlQuery query;
+    if(!query.exec( "CREATE TABLE " T_TECHNOLOGIES " ("
+                            "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                            T_TECHNOLOGIES_NAME " VARCHAR(50) NOT NULL,"
+                            T_TECHNOLOGIES_INFO " TEXT NOT NULL"
+                        " )"
+                    )) {
+        qDebug() << "DataBase: error of create " << T_TECHNOLOGIES;
+        qDebug() << query.lastError().text();
+        return false;
+    } else {
+        return true;
+    }
+    return false;
 }
 
 bool DataBase::createGithubHistoryPoolTable()
 {
-    //
-    return true;
-}
-
-bool DataBase::createContactsTable()
-{
-    //
-    return true;
+    QSqlQuery query;
+    if(!query.exec( "CREATE TABLE " T_GITHUBPOOL " ("
+                            "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                            T_GITHUBPOOL_CURRENCY_ID " INTEGER NOT NULL,"
+                            T_GITHUBPOOL_PROJECT_NAME " VARCHAR(50) NOT NULL,"
+                            T_GITHUBPOOL_LANGUAGE " VARCHAR(20) NOT NULL,"
+                            T_GITHUBPOOL_LICENSE " VARCHAR(50) NOT NULL,"
+                            T_GITHUBPOOL_COMMITS_NUM " INTEGER NOT NULL,"
+                            T_GITHUBPOOL_BRANCHES_NUM " INTEGER NOT NULL,"
+                            T_GITHUBPOOL_RELEASES_NUM " INTEGER NOT NULL,"
+                            T_GITHUBPOOL_CONTRIBUTORS_NUM " INTEGER NOT NULL,"
+                            T_GITHUBPOOL_ISSUSES_OPEN_NUM " INTEGER NOT NULL,"
+                            T_GITHUBPOOL_ISSUSES_CLOSED_NUM " INTEGER NOT NULL,"
+                            T_GITHUBPOOL_PULLREQ_OPEN_NUM " INTEGER NOT NULL,"
+                            T_GITHUBPOOL_PULLREQ_CLOSED_NUM " INTEGER NOT NULL,"
+                            T_GITHUBPOOL_STARS_NUM " INTEGER NOT NULL,"
+                            T_GITHUBPOOL_FORKS_NUM " INTEGER NOT NULL"
+                        " )"
+                    )) {
+        qDebug() << "DataBase: error of create " << T_GITHUBPOOL;
+        qDebug() << query.lastError().text();
+        return false;
+    } else {
+        return true;
+    }
+    return false;
 }
 
 bool DataBase::createMarketsTable()
 {
-    //
-    return true;
+    QSqlQuery query;
+    if(!query.exec( "CREATE TABLE " T_MARKETSPOOL " ("
+                            "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                            T_MARKETSPOOL_CURRENCY_ID " INTEGER NOT NULL,"
+                            T_MARKETSPOOL_MARKET " VARCHAR(20) NOT NULL,"
+                            T_MARKETSPOOL_PAIR " VARCHAR(20) NOT NULL,"
+                            T_MARKETSPOOL_VOL_24 " UNSIGNED BIG INT NOT NULL,"
+                            T_MARKETSPOOL_VOL_P " INTEGER NOT NULL,"
+                            T_MARKETSPOOL_PRICE_USD " INTEGER NOT NULL,"
+                            T_MARKETSPOOL_PRICE_BTC " INTEGER NOT NULL"
+                        " )"
+                    )) {
+        qDebug() << "DataBase: error of create " << T_MARKETSPOOL;
+        qDebug() << query.lastError().text();
+        return false;
+    } else {
+        return true;
+    }
+    return false;
 }
--- database.h
+++ database.h
@@ -22,29 +22,33 @@
 #define DATABASE_NAME           "DataBase.db"
 
 // Таблица: Проекты
-#define T_CURRENCIES_ABLE       "TableCurrencies"
-#define T_CURRENCIES_NAME       "Name"
-#define T_CURRENCIES_SYMBOL     "Symbol"
-#define T_CURRENCIES_PRICE_USD  "PriceUsd"
-#define T_CURRENCIES_PRICE_BTC  "PriceBtc"
-#define T_CURRENCIES_CALC_SUPPLY "CalculatingSupply"
-#define T_CURRENCIES_MINEABLE   "Mineable"
-#define T_CURRENCIES_VOLUME_USD "VolumeUsd"
-#define T_CURRENCIES_VOLUME_BTC "VolumeBtc"
-
-// Таблица: Информация
-#define T_INFO                  "TableBaseInfo"
-#define T_INFO_UPD_DATE         "DateUpdate"
-#define T_INFO_APP_AREAS_ID     "AppAreas_id"
-#define T_INFO_REV_IDEA         "RevolutionIdea"
-#define T_INFO_BASE_PLATFORM_ID "BasePlatform_id"
-#define T_INFO_CON_ALG_ID       "ConsensusAlgorithm_id"
-#define T_INFO_IS_INTER_COMP    "IsInternationalCompany"
-#define T_INFO_IS_WP_IN_CV      "IsWhitepaperInControlVersion"
-#define T_INFO_IS_OP_VACANCIES  "IsOpenVacancies"
-#define T_INFO_WP_URL           "WhitepaperUrl"
-#define T_INFO_RM_URL           "RoadmapUrl"
-#define T_INFO_SITE_URL         "SiteUrl"
+#define T_CURRENCIES                    "TableCurrencies"
+#define T_CURRENCIES_NAME               "Name"
+#define T_CURRENCIES_SYMBOL             "Symbol"
+#define T_CURRENCIES_CALC_SUPPLY        "CalculatingSupply"
+#define T_CURRENCIES_VOLUME_USD         "VolumeUsd"
+#define T_CURRENCIES_VOLUME_BTC         "VolumeBtc"
+#define T_CURRENCIES_PRICE_USD          "PriceUsd"
+#define T_CURRENCIES_PRICE_BTC          "PriceBtc"
+#define T_CURRENCIES_MINEABLE           "Mineable"
+// - manual edit fields
+#define T_CURRENCIES_MANUAL_UPD_DATE    "ManualUpdateDate"
+#define T_CURRENCIES_APP_AREAS_ID       "AppAreas_id"
+#define T_CURRENCIES_REV_IDEA           "RevolutionIdea"
+#define T_CURRENCIES_BASE_PLATFORM_ID   "BasePlatform_id"
+#define T_CURRENCIES_CON_ALG_ID         "ConsensusAlgorithm_id"
+#define T_CURRENCIES_IS_INTER_COMP      "IsInternationalCompany"
+#define T_CURRENCIES_IS_WP_IN_CV        "IsWhitepaperInControlVersion"
+#define T_CURRENCIES_IS_OP_VACANCIES    "IsOpenVacancies"
+#define T_CURRENCIES_WP_URL             "WhitepaperUrl"
+#define T_CURRENCIES_RM_URL             "RoadmapUrl"
+#define T_CURRENCIES_SITE_URL           "SiteUrl"
+// - manual edit fields - contacts
+#define T_CURRENCIES_CONTACTS_TEL          "Phone"
+#define T_CURRENCIES_CONTACTS_MAIL         "Mail"
+#define T_CURRENCIES_CONTACTS_ADDRESS      "Address"
+#define T_CURRENCIES_CONTACTS_SLACK        "Slack"
+#define T_CURRENCIES_CONTACTS_TELEGRAM     "Telegram"
 
 // Таблица: Знаковые даты дорожных карт проектов
 #define T_ROADMAPSDATESPOOL                 "TableRoadmapDatesPool"
@@ -79,27 +83,19 @@
 // Таблица: Оценочные параметры по GitHub
 #define T_GITHUBPOOL                       "TableGithubHistoryPool"
 #define T_GITHUBPOOL_CURRENCY_ID           "Currency_id"
-#define T_GITHUBPOOL_LANGUAGE              "Language"
 #define T_GITHUBPOOL_PROJECT_NAME          "ProjectName"
+#define T_GITHUBPOOL_LANGUAGE              "Language"
+#define T_GITHUBPOOL_LICENSE               "License"
 #define T_GITHUBPOOL_COMMITS_NUM           "CommitsNum"
 #define T_GITHUBPOOL_BRANCHES_NUM          "BranchesNum"
 #define T_GITHUBPOOL_RELEASES_NUM          "ReleasesNum"
 #define T_GITHUBPOOL_CONTRIBUTORS_NUM      "ContributorsNum"
-#define T_GITHUBPOOL_LICENSE               "License"
 #define T_GITHUBPOOL_ISSUSES_OPEN_NUM      "IssusesOpenNum"
 #define T_GITHUBPOOL_ISSUSES_CLOSED_NUM    "IssusesClosedNum"
 #define T_GITHUBPOOL_PULLREQ_OPEN_NUM      "PullRequestOpenNum"
 #define T_GITHUBPOOL_PULLREQ_CLOSED_NUM    "PullRequestClosedNum"
 #define T_GITHUBPOOL_STARS_NUM             "StarsNum"
 #define T_GITHUBPOOL_FORKS_NUM             "ForksNum"
-
-// Таблица: Контакты
-#define T_CONTACTS              "TableContacts"
-#define T_CONTACTS_TEL          "Phone"
-#define T_CONTACTS_MAIL         "Mail"
-#define T_CONTACTS_ADDRESS      "Address"
-#define T_CONTACTS_SLACK        "Slack"
-#define T_CONTACTS_TELEGRAM     "Telegram"
 
 // Таблица: Оценка по биржам
 #define T_MARKETSPOOL                       "TableMarkets"
@@ -121,15 +117,12 @@ public:
      * */
     void connectToDataBase();
     bool inserIntoCurrenciesTable(const QVariantList &data);
-    bool inserIntoInfoTable(const QVariantList &data);
     bool inserIntoRoadmapDatesPoolTable(const QVariantList &data);
     bool inserIntoTokenAlgTable(const QVariantList &data);
     bool inserIntoAppAreasTable(const QVariantList &data);
-    bool inserIntoDevInfoTable(const QVariantList &data);
     bool inserIntoTechnologiesThreadsPoolTable(const QVariantList &data);
     bool inserIntoTechnologiesTable(const QVariantList &data);
     bool inserIntoGithubHistoryPoolTable(const QVariantList &data);
-    bool inserIntoContactsTable(const QVariantList &data);
     bool inserIntoMarketsTable(const QVariantList &data);
 
 private:
@@ -145,7 +138,6 @@ private:
     void closeDataBase();
     bool createAllTables();
     bool createCurrenciesTable();
-    bool createInfoTable();
     bool createRoadmapDatesPoolTable();
     bool createTokenAlgTable();
     bool createAppAreasTable();
@@ -153,7 +145,6 @@ private:
     bool createTechnologiesThreadsPoolTable();
     bool createTechnologiesTable();
     bool createGithubHistoryPoolTable();
-    bool createContactsTable();
     bool createMarketsTable();
 
 signals:
